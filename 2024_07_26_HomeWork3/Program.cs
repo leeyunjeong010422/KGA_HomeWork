a9a541f49e4c31092b1cf0cf831b794c360abda7
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-07-31 12:03:48 +0900

    fix: 코드 변경

--- 2024_07_26_HomeWork3/Program.cs
+++ 2024_07_26_HomeWork3/Program.cs
@@ -1,15 +1,48 @@
-﻿namespace _2024_07_26_HomeWork3
+﻿namespace _2024_07_26_HomeWork3_스택
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Stack<string> stack = new Stack<string>();
+            Console.Write("검사할 괄호를 입력해주세요: ");
+            string input = Console.ReadLine();
 
-
-
+            if (IsParentheses(input))
+            {
+                Console.WriteLine("완성");
+            }
+            else
+            {
+                Console.WriteLine("미완성");
+            }
 
 
         }
+
+        static bool IsParentheses(string input)
+        {
+            int firstParentheses = 0;
+            int secondParentheses = 0;
+            int thirdParentheses = 0;
+
+            foreach (char parentheses in input)
+            {
+                if (parentheses == '(') firstParentheses++;
+                else if (parentheses == '{') secondParentheses++;
+                else if (parentheses == '[') thirdParentheses++;
+                else if (parentheses == ')') firstParentheses--;
+                else if (parentheses == '}') secondParentheses--;
+                else if (parentheses == ']') thirdParentheses--;
+
+
+                if (firstParentheses == 0 && secondParentheses == 0 && thirdParentheses == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
     }
 }
