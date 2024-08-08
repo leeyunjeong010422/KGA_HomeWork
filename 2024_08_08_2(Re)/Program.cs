e322856afbed90d2aa73a84501b9c80a5a8e22c6
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-08-08 18:16:34 +0900

    Add: 빌더패턴코드

---
+++ 2024_08_08_2(Re)/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace 빌드패턴2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HamburgerBuilder bulgogiBurgerBuilder = new HamburgerBuilder();
+            bulgogiBurgerBuilder.SetSource("달콤한 소스");
+            bulgogiBurgerBuilder.SetPatty("불고기 패티");
+
+            HamburgerBuilder shirmpBurgerBuilder = new HamburgerBuilder();
+            shirmpBurgerBuilder.SetBread("호밀빵");
+            shirmpBurgerBuilder.SetSource("마요네즈 소스");
+            shirmpBurgerBuilder.SetPatty("새우 패티");
+
+            //HamburgerBuilder bigmacBuilder = new HamburgerBuilder();
+            //bigmacBuilder.SetBread("참깨빵");
+            //bigmacBuilder.SetPatty("순쇠소기 패티 두장");
+            //bigmacBuilder.SetSource("특별한 소스");
+            //bigmacBuilder.SetVegetable("양상추");
+
+            HamburgerBuilder bigmacBuilder = new HamburgerBuilder();
+            bigmacBuilder.SetBread("참깨빵")
+                .SetPatty("순쇠고기 패티 두장")
+                .SetSource("특별한 소스")
+                .SetVegetable("양상추");
+
+            Hamburger[] burgers = new Hamburger[10];
+            burgers[0] = bulgogiBurgerBuilder.Build();
+            burgers[1] = shirmpBurgerBuilder.Build();
+            burgers[2] = bigmacBuilder.Build();
+
+            for (int i = 0; i < burgers.Length; i++)
+            {
+                Console.WriteLine($"{burgers[i].bread} {burgers[i].source} {burgers[i].patty} {burgers[i].vegetable}");
+            }           
+        }
+
+        public class HamburgerBuilder
+        {
+            public string bread;
+            public string source;
+            public string patty;
+            public string vegetable;
+
+            public HamburgerBuilder()
+            {
+                this.bread = "기본빵";
+                this.source = "케찹";
+                this.patty = "고기패티";
+                this.vegetable = "양상추";
+            }
+
+            public Hamburger Build()
+            {
+                Hamburger hamburger = new Hamburger();
+                hamburger.bread = this.bread;
+                hamburger.source = this.source;
+                hamburger.patty = this.patty;
+                hamburger.vegetable = this.vegetable;
+
+                return hamburger;
+            }
+
+            public HamburgerBuilder SetBread(string bread)
+            {
+                this.bread = bread;
+                return this;
+            }
+
+            public HamburgerBuilder SetSource(string source)
+            {
+                this.source = source;
+                return this;
+            }
+
+            public HamburgerBuilder SetPatty(string patty)
+            {
+                this.patty = patty;
+                return this;
+            }
+
+            public HamburgerBuilder SetVegetable(string vegetable)
+            {
+                this.vegetable = vegetable;
+                return this;
+            }
+        }
+
+        public class Hamburger
+        {
+            public string bread;
+            public string source;
+            public string patty;
+            public string vegetable;
+        }
+    }
+}
---
+++ 2024_08_08_2/Program.cs
@@ -0,0 +1,114 @@
+﻿namespace 빌더패턴
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //안바꾸면 기본 내용들을 사용함
+            OrcBuilder OrcBuilder1 = new OrcBuilder();
+            OrcBuilder1.SetName("전사");
+            OrcBuilder1.SetHP(300);
+
+            OrcBuilder OrcBuilder2 = new OrcBuilder();
+            OrcBuilder2.SetName("궁수");
+            OrcBuilder2.SetWeapon("나무 활");
+            OrcBuilder2.SetLevel(10);
+
+            //OrcBuilder OrcBuilder3 = new OrcBuilder();
+            //OrcBuilder3.SetName("주술사");
+            //OrcBuilder3.SetWeapon("저주 받은 지팡이");
+            //OrcBuilder3.SetArmor("의식복");
+            //OrcBuilder3.SetHP(50);
+            
+            //체인메소드 사용하는 방법
+            OrcBuilder OrcBuilder3 = new OrcBuilder();
+            OrcBuilder3.SetName("주술사").SetWeapon("저주 받은 지팡이").SetArmor("의식복").SetHP(50);
+
+
+            Orc[] orcs = new Orc[9];
+
+            orcs[0] = OrcBuilder2.Build();
+            orcs[1] = OrcBuilder3.Build();
+            orcs[2] = OrcBuilder1.Build();
+
+            OrcBuilder[] builders = new OrcBuilder[9];
+            builders[0] = OrcBuilder1;
+            builders[1] = OrcBuilder2;
+            builders[2] = OrcBuilder3;
+
+            for(int i = 0; i < orcs.Length; i++)
+            {
+                Console.WriteLine($"{orcs[i].name} {orcs[i].weapon} {orcs[i].arrmor} {orcs[i].level} {orcs[i].hp}");
+            }
+        }
+    }
+
+    public class OrcBuilder
+    {
+        public string name;
+        public string weapon;
+        public string arrmor;
+        public int level;
+        public int hp;
+
+        public OrcBuilder()
+        {
+            name = "오크";
+            weapon = "몽둥이";
+            arrmor = "천옷";
+            level = 1;
+            hp = 100;
+        }
+
+        public Orc Build()
+        {
+            Orc orc = new Orc();
+
+            orc.name = name;
+            orc.weapon = weapon;
+            orc.arrmor = arrmor;
+            orc.level = level;
+            orc.hp = hp;
+
+            return orc;
+        }
+
+        public OrcBuilder SetName(string name)
+        {
+            this.name = name;
+            return this;
+        }
+
+        public OrcBuilder SetWeapon(string weapon)
+        {
+            this.weapon = weapon;
+            return this;
+        }
+
+        public OrcBuilder SetArmor(string armor)
+        {
+            this.arrmor = armor;
+            return this;
+        }
+
+        public OrcBuilder SetLevel(int level)
+        {
+            this.level = level;
+            return this;
+        }
+
+        public OrcBuilder SetHP(int hp)
+        {
+            this.hp = hp;
+            return this;
+        }       
+    }
+    public class Orc()
+    {
+        public string name;
+        public string weapon;
+        public string arrmor;
+        public int level;
+        public int hp;
+    }
+}
